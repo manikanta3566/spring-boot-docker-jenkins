cec2c5c5e12627147a6063cf50fd35244dfc3174
Manikanta M <manikantapoojary444@gmail.com>
2022-06-26 15:20:07 +0530

    first commit

---
+++ src/test/java/com/java/springbootapplicationdocker/SpringBootApplicationDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springbootapplicationdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
